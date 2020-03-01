543323bdd399cd480996b7339b03bd19723abbc7
StevenKim <stevenkim@Steven.local>
2020-03-02 02:35:31 +0900

    add ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 21:48:46 by seunkim           #+#    #+#             */
-/*   Updated: 2020/03/01 22:47:29 by seunkim          ###   ########.fr       */
+/*   Updated: 2020/03/02 01:29:57 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,13 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 		ft_memcpy(dest, src, len);
 	else
 	{
-		idx = len - 1;
-		while (idx >= 0)
+		idx = len;
+		while (idx)
 		{	
-			((unsigned char*)dest)[idx] = ((unsigned char*)src)[idx];
 			idx--;
+			((unsigned char*)dest)[idx] = ((unsigned char*)src)[idx];
 		}
 	}
 	return (dest);
 }
 
-#include <string.h>
-#include <stdio.h>
-
-int main(void)
-{
-        char array[10];
-
-        memset(array, 0, sizeof(array));
-        strcpy(array, "test");
-
-        printf("original : %s\n", array);
-
-        ft_memmove(array+2, array, strlen("test"));
-
-        printf("after memmove : %s\n", array);
-
-        return 0;
-}

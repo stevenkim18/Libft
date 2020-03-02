568f00d14e6a7cf1e2cc99cec7631b52835ae899
StevenKim <stevenkim@Steven.local>
2020-03-03 03:58:49 +0900

    add ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,23 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/27 15:38:11 by seunkim           #+#    #+#             */
-/*   Updated: 2020/03/03 03:34:03 by seunkim          ###   ########.fr       */
+/*   Created: 2020/03/03 02:08:43 by seunkim           #+#    #+#             */
+/*   Updated: 2020/03/03 03:58:29 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <stdlib.h>
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
+{	
+	size_t	idx;
+	size_t	dest_len;
+	size_t	src_len;
 
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	idx = 0;
+	while (src[idx] && idx + dest_len + 1 < dstsize)
+	{
+		dest[idx + dest_len] = src[idx];
+		idx++;
+	}
+	if (dstsize < dest_len)
+		return (src_len + dstsize);
+	return (dest_len + src_len);
+}
 
-#endif

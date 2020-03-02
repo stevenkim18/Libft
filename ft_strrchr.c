a482a10663444d9e50c24138d0de6aa66d1dcedc
StevenKim <stevenkim@Steven.local>
2020-03-03 05:11:37 +0900

    edit ft_strchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/03 04:24:26 by seunkim           #+#    #+#             */
-/*   Updated: 2020/03/03 05:11:14 by seunkim          ###   ########.fr       */
+/*   Created: 2020/03/03 04:53:51 by seunkim           #+#    #+#             */
+/*   Updated: 2020/03/03 05:04:36 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(char *s, int c)
+char	*strrchr(char *s, int c)
 {
 	size_t	idx;
-
-	idx = 0;
-	while (s[idx])
-	{
+	size_t	len;
+	
+	len = ft_strlen(s);
+	idx = len - 1;
+	while (idx >= 0)
+	{	
 		if (s[idx] == (char)c)
-			return (s + idx);
-		idx++;
+			return(s + idx);
+		idx--;
 	}
 	return (NULL);
 }
-

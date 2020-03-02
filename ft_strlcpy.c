475b0268b88a0d7f85520cd46f0efc8c2e26dd60
StevenKim <stevenkim@Steven.local>
2020-03-03 01:58:22 +0900

    add ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,22 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/27 15:38:11 by seunkim           #+#    #+#             */
-/*   Updated: 2020/03/03 01:45:01 by seunkim          ###   ########.fr       */
+/*   Created: 2020/03/03 01:11:22 by seunkim           #+#    #+#             */
+/*   Updated: 2020/03/03 01:58:04 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <stdlib.h>
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
+{
+	size_t	idx;
 
-void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlen(const char *s);
+	idx = 0;
+	while (src[idx] && idx + 1 < dstsize)
+	{
+		dest[idx] = src[idx];
+		idx++;
+	}
+	dest[idx] = '\0';
+	return (ft_strlen(src));
+}
 
-#endif

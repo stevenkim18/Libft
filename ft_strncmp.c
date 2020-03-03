c94d9707e865105f364a413ed026da98882bf263
StevenKim <stevenkim@Steven.local>
2020-03-03 17:57:21 +0900

    add ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/03 17:31:14 by seunkim           #+#    #+#             */
+/*   Updated: 2020/03/03 17:57:04 by seunkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	idx;
+
+	idx = 0;
+	while (idx + 1 < n && s1[idx] && s2[idx] && s1[idx] == s2[idx])
+		idx++;
+	return ((unsigned char)s1[idx] - (unsigned char)s2[idx]);
+}
+

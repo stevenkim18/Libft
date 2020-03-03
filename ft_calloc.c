a874f3ab626b113aa2bda5f2529ebe5b01d08491
StevenKim <stevenkim@Steven.local>
2020-03-04 04:57:25 +0900

    add ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/27 15:38:11 by seunkim           #+#    #+#             */
-/*   Updated: 2020/03/04 04:51:37 by seunkim          ###   ########.fr       */
+/*   Created: 2020/03/04 04:18:39 by seunkim           #+#    #+#             */
+/*   Updated: 2020/03/04 04:55:44 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
+	if (!(ptr = malloc(count * size)))
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
 
-#endif

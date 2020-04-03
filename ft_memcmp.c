9975d49eef4f83efe08e093bd0c2019df2ca9625
StevenKim <stevenkim@Steven.local>
2020-04-03 17:49:41 +0900

    norm check

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,13 +6,13 @@
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 03:08:24 by seunkim           #+#    #+#             */
-/*   Updated: 2020/04/03 17:48:31 by seunkim          ###   ########.fr       */
+/*   Updated: 2020/04/03 17:49:29 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int			ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t		idx;
 	unsigned char	*s1_t;
@@ -20,9 +20,9 @@ int			ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	if (n == 0)
 		return (0);
-	idx = 0;
-	s1_t = (unsigned char *)s1;
-	s2_t = (unsigned char *)s2;
+	idx	= 0;
+	s1_t	= (unsigned char *)s1;
+	s2_t 	= (unsigned char *)s2;
 	while (idx + 1 < n && s1_t[idx] && s2_t[idx] && s1_t[idx] == s2_t[idx])
 		idx++;
 	return (s1_t[idx] - s2_t[idx]);

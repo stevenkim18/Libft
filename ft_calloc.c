ec84a2397c3fa15425ef2bc2cefd7b50eaf7b349
Seungwoo Kim <seunkim@c4r6s6.42seoul.kr>
2020-04-03 19:44:22 +0900

    norm check finish, test complete

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 04:18:39 by seunkim           #+#    #+#             */
-/*   Updated: 2020/04/03 17:37:35 by seunkim          ###   ########.fr       */
+/*   Updated: 2020/04/03 19:42:48 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,17 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
+	size_t	idx;
+	char	*tmp;
 
 	if (!(ptr = malloc(count * size)))
 		return (NULL);
-	ft_bzero(ptr, count * size);
+	idx = 0;
+	tmp = (char *)ptr;
+	while (idx < count * size)
+	{
+		tmp[idx] = 0;
+		idx++;
+	}
 	return (ptr);
 }

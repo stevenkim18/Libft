0197d039e28903ede0371afeacb24510496e4a4e
StevenKim <stevenkim@Steven.local>
2020-04-03 17:58:27 +0900

    norm check

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: seunkim <seunkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/02 03:08:24 by seunkim           #+#    #+#             */
-/*   Updated: 2020/04/03 17:55:20 by seunkim          ###   ########.fr       */
+/*   Updated: 2020/04/03 17:58:22 by seunkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	unsigned char	*s1_t;
 	unsigned char	*s2_t;
 
-	if (n == 0)
-		return (0);
 	idx = 0;
 	s1_t = (unsigned char *)s1;
 	s2_t = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
 	while (idx + 1 < n && s1_t[idx] && s2_t[idx] && s1_t[idx] == s2_t[idx])
 		idx++;
 	return (s1_t[idx] - s2_t[idx]);
